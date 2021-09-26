bd2fb0ca529b6ef5e1cc1341f97a3eb99f18e2b7
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-09-26 16:32:33 +0200

    Create search.php

---
+++ search.php
@@ -0,0 +1,40 @@
+<?php
+// Configurazione
+$conf = json_decode(file_get_contents("conf.json"));
+
+if (!empty($conf->header)) {
+   require_once($conf->header);
+}
+ 
+// Pick up data
+$search = $_POST["search"];
+if (!empty($conf->specificFile)) {
+    $ext = "*.$conf->specificFile";
+} else {
+    $ext = "*";
+}
+
+if ($conf->goDown === true) {
+   $dow = "<br>";
+} else {
+   $down = "";
+}
+
+$dir = $conf->searchDir:
+// Load options
+foreach (glob("$dir/*$ext") as $file) {
+    if (!empty($conf->limitOutput) && $conf->limitOutput > 0 && $rep < $limitOutput) {
+      if ($conf->underlined === true) {
+        $file = str_replace($search, '<u>' .$search. '</u>', $file);
+      }
+      if ($conf->showDir === false) {
+        $file = str_replace($dir, "", $file);
+      }
+      if (!empty($conf->redi)) {
+         $fileredi = str_replace("%file%", $file, $conf->redi);
+         echo "<a href='$fileredi'>$file</a>$down";
+      } else {
+         echo "$file$down";
+      }
+     }
+}
